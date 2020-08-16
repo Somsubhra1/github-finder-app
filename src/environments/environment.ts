8efeada71005ad5f058b3281840beb78603e3729
Somsubhra Das <das.somsubhra1@gmail.com>
2020-08-17 00:00:27 +0530

    added firebase config to enviroment variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAZaKLU5oQ0x2MToI9hHjX1LXZ2AesVN8s',
+    authDomain: 'angular-github-searcher.firebaseapp.com',
+    databaseURL: 'https://angular-github-searcher.firebaseio.com',
+    projectId: 'angular-github-searcher',
+    storageBucket: 'angular-github-searcher.appspot.com',
+    messagingSenderId: '590810266033',
+    appId: '1:590810266033:web:d09fd9f23703ebe4dae590',
+    measurementId: 'G-LT8YFRHLKX',
+  },
 };
 
 /*
