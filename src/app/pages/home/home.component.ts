b3675b95bdb2a8bfc37d2f88f1cf992e82413581
Somsubhra Das <das.somsubhra1@gmail.com>
2020-08-18 23:02:48 +0530

    home component done

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { GithubService } from '../../services/github.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  user = null;
+  userName: string;
+  error = null;
 
-  constructor() { }
+  constructor(
+    private githubService: GithubService,
+    private ref: ChangeDetectorRef
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  handleFind() {
+    this.githubService.getUserDetails(this.userName).subscribe(
+      (user) => {
+        this.user = user;
+        this.error = null;
+        this.ref.detectChanges();
+      },
+      (error) => {
+        this.user = null;
+        this.error = 'User not found';
+        this.ref.detectChanges();
+      }
+    );
   }
-
 }
