81710a9f929fd73318e22b9385238c59d5965525
Somsubhra Das <das.somsubhra1@gmail.com>
2020-08-19 20:49:42 +0530

    making app production ready

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAZaKLU5oQ0x2MToI9hHjX1LXZ2AesVN8s',
+    authDomain: 'angular-github-searcher.firebaseapp.com',
+    databaseURL: 'https://angular-github-searcher.firebaseio.com',
+    projectId: 'angular-github-searcher',
+    storageBucket: 'angular-github-searcher.appspot.com',
+    messagingSenderId: '590810266033',
+    appId: '1:590810266033:web:d09fd9f23703ebe4dae590',
+    measurementId: 'G-LT8YFRHLKX',
+  },
 };
